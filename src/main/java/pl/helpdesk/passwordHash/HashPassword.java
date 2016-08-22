f5379a4d784c1128f95d86ad123462febc33be54
Adam Ulidowski <adam.ulidowski@wp.pl>
2016-08-22 21:17:17 +0200

    Dodanie Serializacji do entitów

---
+++ src/main/java/pl/helpdesk/passwordHash/HashPassword.java
@@ -0,0 +1,21 @@
+package pl.helpdesk.passwordHash;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public abstract class HashPassword {
+
+	public static String PasswordHash(String password) throws NoSuchAlgorithmException {
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(password.getBytes());
+
+		byte byteData[] = md.digest();
+
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
+	}
+}
---
+++ src/main/java/pl/helpdesk/validation/Validation.java
@@ -0,0 +1,45 @@
+package pl.helpdesk.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Validation {
+	/**
+	 * Waliduje adres email.
+	 * 
+	 * @param email Ciąg znaków do spradzenia.
+	 * @return True, jeżeli email jest poprawny. W przeciwnym wypadku false.
+	 */
+	public static boolean emailValidate(String email) {
+
+		Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+		Matcher m = p.matcher(email);
+
+		boolean matchFound = m.matches();
+
+		if (matchFound) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Waliduje nazwy zawierające jedynie litery, włączając polskie znaki.
+	 * 
+	 * @param name Ciąg znaków do alidacji.
+	 * @return True, jeżeli ciąg znaków jest poprawny. W przeciwnym wypadku false.
+	 */
+	public static boolean nameValidate(String name) {
+
+		String illegal_regex = "[^a-zA-ZąćęłńóśźżĄĘŁŃÓŚŹŻ]";
+		Pattern p = Pattern.compile(illegal_regex);
+		Matcher m = p.matcher(name);
+
+		if (m.find()) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
