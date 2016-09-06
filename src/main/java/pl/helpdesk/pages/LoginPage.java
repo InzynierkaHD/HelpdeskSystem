4338a3f7d5f84c54fd5af2cc85220982e4a6ceed
Adam Ulidowski <adam.ulidowski@wp.pl>
2016-09-06 12:21:39 +0200

    Jeszcze jedna modyfikacja panelu logowania

--- src/main/java/pl/helpdesk/pages/LoginPage.java
+++ src/main/java/pl/helpdesk/pages/LoginPage.java
@@ -26,42 +26,37 @@ public class LoginPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-
-	
 	@SpringBean
 	private IUserDao userSpring;
-	
+
 	@SpringBean
 	private IAdminDao adminDao;
-	
+
 	@SpringBean
 	private IAgentDao agentDao;
-	
+
 	@SpringBean
 	private IEmployeeDao employeeDao;
-	
+
 	@SpringBean
 	private IClientDao clientDao;
 
 	@SpringBean
 	private ILoggingHistoryDao loggingHistoryDao;
-	
+
 	private User userDataModel = new User();
 
 	public LoginPage() {
 		final TextField<String> login = new TextField<String>("login",
 				new PropertyModel<String>(userDataModel, "login"));
-		
+
 		PasswordTextField haslo = new PasswordTextField("haslo", new PropertyModel<String>(userDataModel, "haslo"));
 
 		final Label badInfo = new Label("message", "Złe dane!");
 		badInfo.setVisible(Boolean.FALSE);
-		
+
 		final Label userBlocked = new Label("blocked", "Konto zablokowane!");
 		userBlocked.setVisible(Boolean.FALSE);
-		
-		
-		
 
 		Form<?> form = new Form<Void>("form") {
 			/**
@@ -73,64 +68,36 @@ public class LoginPage extends WebPage {
 			public void onSubmit() {
 
 				super.onSubmit();
-				User findedUser=null;
-				LoggingHistory loggingHistory=new LoggingHistory();
+				User findedUser = null;
+				LoggingHistory loggingHistory = new LoggingHistory();
 				try {
-					findedUser = userSpring.findUserByLoginAndPassword(userDataModel.getLogin(), userDataModel.getHaslo());
+					findedUser = userSpring.findUserByLoginAndPassword(userDataModel.getLogin(),
+							userDataModel.getHaslo());
 				} catch (HibernateException | NoSuchAlgorithmException e) {
 					e.printStackTrace();
 				}
-				if(findedUser != null){
-					Date date=new Date();
+				if (findedUser != null) {
+					Date date = new Date();
 					loggingHistory.setDataLogowania(date);
 					loggingHistory.setUserDataModel(findedUser);
 					loggingHistoryDao.save(loggingHistory);
 					findedUser.setOst_logowanie(date);
 					userSpring.update(findedUser);
 					ApplicationSession.getInstance().setUser(findedUser);
-					if(adminDao.isAdmin(findedUser)){
+					if (adminDao.isAdmin(findedUser)) {
 						setResponsePage(AdminFinalPage.class);
-					} else if (employeeDao.isEmployee(findedUser)){
+					} else if (employeeDao.isEmployee(findedUser)) {
 						setResponsePage(EmployeeFinalPage.class);
-					} else if(agentDao.isAgent(findedUser)){
+					} else if (agentDao.isAgent(findedUser)) {
 						setResponsePage(AgentFinalPage.class);
-					} else if(clientDao.isClient(findedUser)){
+					} else if (clientDao.isClient(findedUser)) {
 						setResponsePage(ClientFinalPage.class);
 					}
-					
-				}
-				else{
+
+				} else {
 					System.out.println("Bledne dane");
 					badInfo.setVisible(Boolean.TRUE);
 				}
-				
-				/*userDao = new UserDao();
-				badInfo.setVisible(Boolean.FALSE);
-				userBlocked.setVisible(Boolean.FALSE);
-				try {
-					if ((userDao.findUserByLoginAndPassword(userDataModel.getLogin(), userDataModel.getHaslo())
-							.equals("none"))) {
-						badInfo.setVisible(Boolean.TRUE);
-					} else if(userDao.czyBlokowany(userDataModel.getLogin())==true){						
-						userBlocked.setVisible(Boolean.TRUE);
-					} else {
-						UserSession.getInstance().setuSerDataModel(userDataModel);
-						UserSession.getInstance().setAttribute("id",
-								userDao.findUserId(UserSession.getInstance().getuSerDataModel().getLogin()));
-						UserSession.getInstance().setAttribute("blok",
-								userDao.czyBlokowany(UserSession.getInstance().getuSerDataModel().getLogin()));
-						if (userDao.userType(UserSession.getInstance().getuSerDataModel().getLogin()) == 4) {
-							setResponsePage(AdminPage.class);
-						} else if (userDao.userType(UserSession.getInstance().getuSerDataModel().getLogin()) == 1){
-							setResponsePage(EmployeePage.class);
-						} else {
-							setResponsePage(UserPage.class);
-						}
-					}
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
-				}*/
-
 			}
 
 		};
