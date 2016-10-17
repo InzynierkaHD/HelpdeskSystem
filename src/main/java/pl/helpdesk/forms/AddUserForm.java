1757ea30f86561d53cfcddf470cc15cc8782bda7
Adam Ulidowski <adam.ulidowski@wp.pl>
2016-10-17 18:56:35 +0200

    Zrobienie dodawania użytkowników, kompletne

--- src/main/java/pl/helpdesk/forms/AddUserForm.java
+++ src/main/java/pl/helpdesk/forms/AddUserForm.java
@@ -49,75 +49,47 @@ public class AddUserForm extends Panel {
 
 	private String selectedCompany;
 	private Agent agent;
+	private User userDataModel;
+
+	private Label badName;
+	private Label badSurname;
+	private Label badEmail;
+	private Label loginLength;
+	private Label passLength;
+	private Label userExist;
+	private Label emailExist;
+	private Label przeszlo;
+	private Label tooMany;
+	private Label companyFail;
+	private Label company;
+
+	private SelectForm selectCompany;
+	private TextField<String> imie;
+	private TextField<String> nazwisko;
+	private TextField<String> email;
+	private TextField<String> login;
+	private PasswordTextField haslo;
+	private Form<?> creating;
+
+	
 
 	private static final long serialVersionUID = 1L;
 
 	public AddUserForm(String id, final String userType) {
 		super(id);
+		CreateMessageLabels();
+		setMessageLabelsInvisible(userType);
 
-		final Label badName = new Label("badname", "Wpisz poprawnie imię!");
-		badName.setVisible(false);
-		final Label badSurname = new Label("badsurname", "Wpisz poprawnie nazwisko!");
-		badSurname.setVisible(false);
-		final Label badEmail = new Label("bademail", "Wpisz poprawnie email!");
-		badEmail.setVisible(false);
-		final Label loginLength = new Label("loginlength", "Login musi zawierac od 4 do 15 znaków!");
-		loginLength.setVisible(false);
-		final Label passLength = new Label("passlength", "Hasło musi zawierac od 4 do 15 znaków!");
-		passLength.setVisible(false);
-		final Label userExist = new Label("userExist", "Użytkownik o podanym loginie istnieje w systemie!");
-		userExist.setVisible(false);
-		final Label emailExist = new Label("emailExist", "Użytkownik o podanym adresie istnieje w systemie!");
-		emailExist.setVisible(false);
-		final Label przeszlo = new Label("przeszlo", "Dodano użytkownika!");
-		przeszlo.setVisible(false);
-		final Label tooMany = new Label("tooMany", "W twojej firmie jest już zarejestrowane 5 kont klienckich!");
-		tooMany.setVisible(false);
-		final Label companyFail = new Label("companyFail", "Musisz wybrać firmę!");
-		companyFail.setVisible(false);
+		createFormComponents(userType);
 
-		User userDataModel = new User();
+		creating = new Form("creating") {
 
-		final Label company = new Label("company", "Firma:");
-		final SelectForm selectCompany = new SelectForm("selectCompany",
-				new PropertyModel<String>(this, "selectedCompany"), companyDao.getAllToString());
-		selectCompany.setVisible(false);
-		company.setVisible(false);
-		if (userType.equals("agent")) {
-			selectCompany.setVisible(true);
-			company.setVisible(true);
-		}
-
-		final TextField<String> imie = new TextField<String>("imie", new PropertyModel<String>(userDataModel, "imie"));
-		final TextField<String> nazwisko = new TextField<String>("nazwisko",
-				new PropertyModel<String>(userDataModel, "nazwisko"));
-		final TextField<String> email = new TextField<String>("email",
-				new PropertyModel<String>(userDataModel, "email"));
-		final TextField<String> login = new TextField<String>("login",
-				new PropertyModel<String>(userDataModel, "login"));
-
-		final PasswordTextField haslo = new PasswordTextField("haslo",
-				new PropertyModel<String>(userDataModel, "haslo"));
-
-		Form<?> creating = new Form("creating") {
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public void onSubmit() {
 				super.onSubmit();
-				tooMany.setVisible(false);
-				badName.setVisible(false);
-				badSurname.setVisible(false);
-				badEmail.setVisible(false);
-				loginLength.setVisible(false);
-				passLength.setVisible(false);
-				userExist.setVisible(false);
-				emailExist.setVisible(false);
-				przeszlo.setVisible(false);
-				companyFail.setVisible(false);
+				setMessageLabelsInvisible(userType);
 
 				String imie2 = imie.getInput();
 				String nazwisko2 = nazwisko.getInput();
@@ -130,20 +102,17 @@ public class AddUserForm extends Panel {
 				} catch (NoSuchAlgorithmException e1) {
 					e1.printStackTrace();
 				}
-
 				boolean IsOk = true;
 				if (Validation.nameValidate(imie2) == false || imie2.length() < 2 || imie2.length() > 20
 						|| imie2.isEmpty()) {
 					badName.setVisible(true);
 					IsOk = false;
 				}
-
 				if (Validation.nameValidate(nazwisko2) == false || nazwisko2.length() < 2 || nazwisko2.length() > 20
 						|| nazwisko2.isEmpty()) {
 					badSurname.setVisible(true);
 					IsOk = false;
 				}
-
 				if (Validation.emailValidate(email2) == false) {
 					badEmail.setVisible(true);
 					IsOk = false;
@@ -151,7 +120,6 @@ public class AddUserForm extends Panel {
 					emailExist.setVisible(true);
 					IsOk = false;
 				}
-
 				if (login2.length() < 4 || login2.length() > 15 || login2.isEmpty()) {
 					loginLength.setVisible(true);
 					IsOk = false;
@@ -159,7 +127,6 @@ public class AddUserForm extends Panel {
 					userExist.setVisible(true);
 					IsOk = false;
 				}
-
 				if (haslo2.length() < 4 || haslo2.length() > 15 || haslo2.isEmpty()) {
 					passLength.setVisible(true);
 					IsOk = false;
@@ -168,7 +135,6 @@ public class AddUserForm extends Panel {
 					companyFail.setVisible(true);
 					IsOk = false;
 				}
-
 				if (userType.equals("client")) {
 					agent = agentDao.findAgentByUser(ApplicationSession.getInstance().getUser());
 					int numberOfActiveClients = 0;
@@ -183,7 +149,6 @@ public class AddUserForm extends Panel {
 						IsOk = false;
 					}
 				}
-
 				if (IsOk) {
 					User newUser = new User(login2, hasloHash, imie2, nazwisko2, email2, false, false, 0);
 					userSpring.save(newUser);
@@ -206,6 +171,69 @@ public class AddUserForm extends Panel {
 
 			}
 		};
+
+		AddMessageLabels();
+		AddForm();
+	}
+	
+	private void CreateMessageLabels() {
+		badName = new Label("badname", "Wpisz poprawnie imię!");
+		badSurname = new Label("badsurname", "Wpisz poprawnie nazwisko!");
+		badEmail = new Label("bademail", "Wpisz poprawnie email!");
+		loginLength = new Label("loginlength", "Login musi zawierac od 4 do 15 znaków!");
+		passLength = new Label("passlength", "Hasło musi zawierac od 4 do 15 znaków!");
+		userExist = new Label("userExist", "Użytkownik o podanym loginie istnieje w systemie!");
+		emailExist = new Label("emailExist", "Użytkownik o podanym adresie istnieje w systemie!");
+		przeszlo = new Label("przeszlo", "Dodano użytkownika!");
+		tooMany = new Label("tooMany", "W twojej firmie jest już zarejestrowane 5 kont klienckich!");
+		companyFail = new Label("companyFail", "Musisz wybrać firmę!");
+		company = new Label("company", "Firma:");
+	}
+
+	private void setMessageLabelsInvisible(final String userType) {
+		badName.setVisible(false);
+		badSurname.setVisible(false);
+		badEmail.setVisible(false);
+		loginLength.setVisible(false);
+		passLength.setVisible(false);
+		userExist.setVisible(false);
+		emailExist.setVisible(false);
+		company.setVisible(false);
+		przeszlo.setVisible(false);
+		tooMany.setVisible(false);
+		companyFail.setVisible(false);
+		if (userType.equals("agent")) {
+			company.setVisible(true);
+		}
+	}
+
+	private void createFormComponents(final String userType) {
+		userDataModel = new User();
+		selectCompany = new SelectForm("selectCompany", new PropertyModel<String>(this, "selectedCompany"),
+				companyDao.getAllToString());
+		selectCompany.setVisible(false);
+		if (userType.equals("agent")) {
+			selectCompany.setVisible(true);
+		}
+		imie = new TextField<String>("imie", new PropertyModel<String>(userDataModel, "imie"));
+		nazwisko = new TextField<String>("nazwisko", new PropertyModel<String>(userDataModel, "nazwisko"));
+		email = new TextField<String>("email", new PropertyModel<String>(userDataModel, "email"));
+		login = new TextField<String>("login", new PropertyModel<String>(userDataModel, "login"));
+		haslo = new PasswordTextField("haslo", new PropertyModel<String>(userDataModel, "haslo"));
+	}
+	
+	private void AddForm() {
+		add(creating);
+		creating.add(login);
+		creating.add(imie);
+		creating.add(nazwisko);
+		creating.add(email);
+		creating.add(haslo);
+		creating.add(selectCompany);
+		creating.add(company);
+	}
+
+	private void AddMessageLabels() {
 		creating.add(badName);
 		creating.add(badSurname);
 		creating.add(badEmail);
@@ -216,15 +244,5 @@ public class AddUserForm extends Panel {
 		creating.add(przeszlo);
 		creating.add(tooMany);
 		creating.add(companyFail);
-
-		add(creating);
-		creating.add(login);
-		creating.add(imie);
-		creating.add(nazwisko);
-		creating.add(email);
-		creating.add(haslo);
-		creating.add(selectCompany);
-		creating.add(company);
-
 	}
 }
